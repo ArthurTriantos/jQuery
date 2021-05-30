42f43af82ebeea84ceaa2136d96cc7b28fb8776e
Arthur <arttriantos@gmail.com>
2021-05-30 16:55:10 -0500

    button functionality

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ $(document).ready (function () {
            alert($("input:text").val());
        });
     });
+    $(':input[type="submit"]').prop('disabled', true);
+    $('input[type="text"]').keyup(function() {
+       if($(this).val() != '') {
+          $(':input[type="submit"]').prop('disabled', false);
+       }
+    });
 });
